6afcb23b148171ea057f2338c7e1b87fee59d011
shreyansh <shreyanshrpatel123@gmail.com>
2024-07-15 11:42:15 +0530

    connect db

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB).then(()=> {
+    console.log('Connected to MongoDB');  
+    }).catch((err) => {
+        console.error(err);
+    }
+);
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
