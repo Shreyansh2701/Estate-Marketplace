68724bee3c2f5d55c072de95180fc2d1ef49555c
shreyansh <shreyanshrpatel123@gmail.com>
2024-07-30 11:50:50 +0530

    complete signup functnality

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -2,7 +2,6 @@ const User = require("../models/user.model");
 const bcrypt = require("bcryptjs");   
 
 const signup = async (req, res, next) => {
-    console.log(req.body);
     const { username, email, password } = req.body;
     const hashedPassword = bcrypt.hashSync(password, 10);
     const newUser = new User({ username, email, password: hashedPassword });
