03b69defdbd4001610629b4f9c8edc8714cd2047
shreyansh <shreyanshrpatel123@gmail.com>
2024-07-30 12:15:48 +0530

    create signin api

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 const User = require("../models/user.model");
 const bcrypt = require("bcryptjs");   
+const errorHandler = require("../utils/error");
+const jwt = require("jsonwebtoken");
 
 const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -13,4 +15,23 @@ const signup = async (req, res, next) => {
     }
 }
 
-module.exports = signup;
+const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({message: 'User not found'});
+        }
+        const isMatch = bcrypt.compareSync(password, user.password);
+        if (!isMatch) {
+            return next(errorHandler(401, 'Wrong credentials.'));
+        }
+        const token = jwt.sign({ id: user._id}, process.env.JWT_SECRET);
+        const {password: pass , ...rest} = user._doc;
+        res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest);
+    } catch (error) {
+        next(error);
+    }
+}
+
+module.exports = { signup, signin };
